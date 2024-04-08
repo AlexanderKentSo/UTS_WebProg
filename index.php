055f940098f2a27ee99173376a4e9a796567c6f7
AlexanderKentSo <alexanderkentso96@gmail.com>
2024-04-08 20:04:38 +0700

    revisi

--- index.php
+++ index.php
@@ -6,17 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
     <style>
-        body{
+        body {
             background-color: lightblue;
         }
-        h1{
+
+        h1 {
             color: blue;
             text-align: center;
         }
-        p{
+
+        p {
             color: black;
             font-size: 20px;
         }
+
         <?php
 
         ?>
@@ -30,17 +33,15 @@
     <select name="theme" id="themeBox">
         <option value="">--Choose Theme--</option>
         <?php
-        if(isset($_COOKIE["theme"]))
-        {
-            $arrTheme = $_COOKIE["theme"]; 
-            foreach($arrTheme as $arr)
-            {
-                echo '<option value="'.$arr["nama"].'">'.$arr["nama"].'</option>';
+        if (isset($_COOKIE["theme"])) {
+            $arrTheme = $_COOKIE["theme"];
+            foreach ($arrTheme as $arr) {
+                echo '<option value="' . $arr["nama"] . '">' . $arr["nama"] . '</option>';
             }
         }
         ?>
     </select>
-    <a style="text-decoration: underline;">Add new Theme</a>
+    <a style="text-decoration: underline;" href="tambah.php">Add new Theme</a>
     <br>
     <p>
     <form method="post" action="index.php">
--- tambah.php
+++ tambah.php
@@ -1,5 +1,24 @@
 <?php
 $themes = array();
+if (isset($_COOKIE["theme"])) {
+    $th = $_COOKIE["theme"];
+    echo "<p>$th</p>";
+}
+
+if (isset($_POST["ok"])) {
+    $theme = $_POST["theme"];
+    $background = $_POST["background"];
+    $heading = $_POST["heading"];
+    $alignment = $_POST["aligment"];
+    $paragraph = $_POST["paragraph"];
+    $size = $_POST["size"];
+    setcookie("theme", $theme, time() + 600);
+    setcookie("background", $background, time() + 600);
+    setcookie("heading", $heading, time() + 600);
+    setcookie("aligment", $alignment, time() + 600);
+    setcookie("paragraph", $paragraph, time() + 600);
+    setcookie("size", $size, time() + 600);
+}
 ?>
 
 <!DOCTYPE html>
@@ -12,12 +31,13 @@ $themes = array();
 </head>
 
 <body>
-    <form action="add.php" method="post">
+    <form action="tambah.php" method="post">
         <p><label for="theme">Name of your theme: </label><input type="text" name="theme" id="theme"></p>
         <p><label for="background">Color of Page Background: </label><input type="color" name="background" id="background"></p>
         <p><label for="heading">Color of Heading: </label><input type="color" name="heading" id="heading"></p>
         <p><label for="alignment">Alignment of Heading: </label>
             <select name="alignment" id="alignment">
+                <option value="">--select alignment--</option>
                 <option value="center">Center</option>
                 <option value="left">Left</option>
                 <option value="Right">Right</option>
@@ -26,7 +46,7 @@ $themes = array();
         <p><label for="paragraph">Color of Paragraph: </label><input type="color" name="paragraph" id="paragraph"></p>
         <p><label for="size">Font Size of your Paragraph: </label><input type="number" name="size" id="size" min="0">
         <p>
-            <button type="submit" name="btnSimpan" value="ok">SAVE</button>
+            <button type="submit" name="ok" value="ok">SAVE</button>
         </p>
     </form>
 </body>
